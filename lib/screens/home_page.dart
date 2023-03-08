88bf5e54373fac7268b855477a3e75bafa5fdf16
DanPace725 <118482982+DanPace725@users.noreply.github.com>
2023-03-07 18:59:53 -0800

    Update App design

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -22,37 +22,57 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.grey[800], // Set app bar background color
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30), // Set rounded corner radius
+          ),
+        ),
         title: const Text('Home Page'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Welcome to the Home Page!',
-            style: TextStyle(fontSize: 24),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 250,
-            child: TextField(
-              controller: _textEditingController,
-              decoration: const InputDecoration(
-                hintText: 'Enter some text',
+      backgroundColor: Colors.grey[900], // Set background color
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to the Home Page!',
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 250, // Set border radius  of text field
+              child: TextField(
+                controller: _textEditingController,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  hintText: 'Enter some text',
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _textList.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_textList[index]),
-                );
-              },
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _textList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_textList[index]),
+                    textColor: Colors.white,
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
